92f90e8aa0777d68a8d339b14993bf5db082aa2b
NgKien94 <quangkienofqn@gmail.com>
2025-02-10 22:17:47 +0700

    Optimize upload files use multer

--- routes/admin/products-route.js
+++ routes/admin/products-route.js
@@ -1,22 +1,11 @@
 const express = require('express')
 const controller = require('../../controllers/admin/products-controller')
-const path = require('path')
+const multerStorageHelper = require('../../helpers/multerStorage')
 const multer  = require('multer')
 const route = express.Router();
 
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.resolve(__dirname,'../../public/uploads/'))
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now()
-
-      cb(null,  uniqueSuffix + '-'+file.originalname)
-    }
-  })
-  
-const upload = multer({ storage: storage })
+const upload = multer({ storage: multerStorageHelper() })
 
 //GET PRODUCTS
 route.get('/', controller.index)
