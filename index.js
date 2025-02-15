eb0a690b0ed66466989f52ce39435d0ff70afceb
NgKien94 <quangkienofqn@gmail.com>
2025-02-15 21:53:47 +0700

    wtf?? connection string

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT
-//app.set('views', './views')
+
 app.set('views',`${__dirname}/views`)
 app.set('view engine', 'pug')
 
-// app.use(express.static('public'))
 app.use(express.static(`${__dirname}/public`))
 app.use(methodOverride('_method'))
 
