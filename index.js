2dd54313ae494199308e341aa6916f47030220ca
NgKien94 <quangkienofqn@gmail.com>
2025-02-15 21:45:40 +0700

    Config vercel.json to deploy

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT
-app.set('views', './views')
+//app.set('views', './views')
+app.set('views',`${__dirname}/views`)
 app.set('view engine', 'pug')
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(express.static(`${__dirname}/public`))
 app.use(methodOverride('_method'))
 
 
