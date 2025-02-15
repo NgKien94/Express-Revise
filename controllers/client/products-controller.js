06d7cc77fb67a55c5d39514539027a85017299d6
NgKien94 <quangkienofqn@gmail.com>
2025-02-15 20:33:33 +0700

    Display information of product - role Client

--- controllers/client/products-controller.js
+++ controllers/client/products-controller.js
@@ -1,26 +1,47 @@
 const Product = require('../../models/product-model');
 
-module.exports = {
-    index: async (req,res)=>{
+module.exports.index = async (req, res) => {
 
-        const listProducts = await Product
+    const listProducts = await Product
         .find({
             deleted: false,
             status: 'active'
         })
-        .sort({position: "desc"})
-        
-        const newListProducts = listProducts.map( (item) =>{
-            
-                item.newPrice = (item.price - item.price * (item.discountPercentage/100)).toFixed(2)
-                return item;
-        })
+        .sort({ position: "desc" })
+
+    const newListProducts = listProducts.map((item) => {
+
+        item.newPrice = (item.price - item.price * (item.discountPercentage / 100)).toFixed(2)
+        return item;
+    })
 
 
-        res.render('client/pages/products/index.pug',{
-            pageTitle: 'Product Page',
-            content: "Products page",
-            products: newListProducts
+    res.render('client/pages/products/index.pug', {
+        pageTitle: 'Product Page',
+        content: "Products page",
+        products: newListProducts
+    })
+}
+
+module.exports.detail = async (req, res) => {
+    try {
+        const slug = req.params.slug
+
+        let findObject = {
+            slug: slug,
+            deleted: false,
+            status: "active"
+        }
+
+        const product = await Product.findOne(findObject);
+
+
+        res.render('client/pages/products/detail', {
+            pageTitle: product.title,
+            product: product
         })
+    } catch (error) {
+        console.log(error)
+        res.redirect('/products')
     }
 }
--- routes/client/products-route.js
+++ routes/client/products-route.js
@@ -2,6 +2,12 @@ const express = require('express');
 const controller = require('../../controllers/client/products-controller')
 const route = express.Router();
 
+// [GET] View homepage client
 route.get('/',controller.index)
+
+
+//[GET] View list of products client 
+
+route.get('/detail/:slug',controller.detail)
 
 module.exports = route;
