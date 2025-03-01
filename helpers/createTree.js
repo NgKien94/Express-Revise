a45136f395c5a8b317d588ac0088aaf46028cf91
NgKien94 <quangkienofqn@gmail.com>
2025-03-01 18:58:26 +0700

    Edit number order

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,13 +1,18 @@
 // Tree category
+let count = 0;
 function createTree(items, parentId = "") {
     const tree = []
     items.forEach((item) => {
-        const newItem = item;
-        if (item.parent_id == parentId) {
+
+        if (item.parent_id === parentId) {
+            const newItem = item;
+            count++;
+            newItem.index = count;
             const children = createTree(items, item.id)
             if (children.length > 0) {
                 newItem.children = children;
             }
+
             tree.push(newItem);
         }
 
@@ -17,6 +22,7 @@ function createTree(items, parentId = "") {
 //End tree category
 
 module.exports.tree = (items,parentId="") =>{
+    count = 0;
     const tree = createTree(items,parentId)
     return tree;
 }
