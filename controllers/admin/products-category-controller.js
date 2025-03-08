8e6018af69f97ccd917147dc69af549cbde8550a
NgKien94 <quangkienofqn@gmail.com>
2025-03-08 10:15:18 +0700

    Edit category Functional

--- controllers/admin/products-category-controller.js
+++ controllers/admin/products-category-controller.js
@@ -3,40 +3,40 @@ const systemConfig = require('../../configs/system')
 const createTreeHelper = require('../../helpers/createTree')
 
 // [GET] /products-category
-module.exports.index = async (req,res) =>{
-    let listCategory =  {
-        deleted : false
+module.exports.index = async (req, res) => {
+    let listCategory = {
+        deleted: false
     }
 
     const records = await ProductCategory.find(listCategory);
     const newRecords = createTreeHelper.tree(records)
 
 
-    res.render('admin/pages/category/index.pug',{
+    res.render('admin/pages/category/index.pug', {
         pageTitle: 'Danh mục sản phẩm',
         records: newRecords
     })
 }
 
 // [GET] /products-category/viewCreate-category
-module.exports.viewCreate = async (req,res) =>{
+module.exports.viewCreate = async (req, res) => {
     let allCategory = {
         deleted: false
     }
 
-    const records  = await ProductCategory.find(allCategory)
+    const records = await ProductCategory.find(allCategory)
     const newRecords = createTreeHelper.tree(records)
 
 
-    res.render('admin/pages/category/create.pug',{
+    res.render('admin/pages/category/create.pug', {
         pageTitle: 'Thêm danh mục sản phẩm',
         records: newRecords
     })
 }
 
 // [POST] /products-category/create-category
-module.exports.create = async (req,res) =>{
-    
+module.exports.create = async (req, res) => {
+
     try {
 
         //handle position
@@ -48,10 +48,10 @@ module.exports.create = async (req,res) =>{
         }
         //end handle position
 
-        if(!req.body.thumbnail){
+        if (!req.body.thumbnail) {
             req.body.thumbnail = ""
         }
-      
+
 
         const newProduct = new ProductCategory(req.body)
         await newProduct.save()
@@ -66,4 +66,41 @@ module.exports.create = async (req,res) =>{
         res.redirect(`${systemConfig.prefixAdmin}/products-category`)
     }
 
+}
+
+// [GET] /products-category/edit/:id
+module.exports.viewEdit = async (req, res) => {
+    const id = req.params.id
+
+    const record = await ProductCategory.findOne({ _id: id });
+    const allRecord = await ProductCategory.find({ deleted: false });
+    const listNode = createTreeHelper.tree(allRecord);
+
+    res.render('admin/pages/category/edit.pug', {
+        pageTitle: 'Chỉnh sửa danh mục',
+        record: record,
+        listNode: listNode
+    })
+}
+
+
+// [PATCH] /products-category/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id
+        if(req.body.position){
+            req.body.position = parseInt(req.body.position)
+        }
+        else{
+            req.body.position = await ProductCategory.countDocuments() + 1
+        }
+        await ProductCategory.updateOne({ _id: id }, req.body)
+        req.flash('success', "Chỉnh sửa danh mục thành công")
+        res.redirect(req.get('Referrer') || `${systemConfig.prefixAdmin}/products-category`)
+  
+    } catch (error) {
+        console.log(error)
+        req.flash('error', "Có lỗi xảy ra khi chỉnh sửa danh mục vui lòng thử lại sau")
+        res.redirect(`${systemConfig.prefixAdmin}/products-category`)
+    }
 }
--- routes/admin/products-category-route.js
+++ routes/admin/products-category-route.js
@@ -11,10 +11,10 @@ const middlewareUploadCloud = require('../../middlewares/admin/uploadCloud-middl
 
 
 //[GET] /products-category
-route.get('/',controller.index)
+route.get('/', controller.index)
 
 //[GET] /products-category/viewCreate-category
-route.get('/viewCreate-category',controller.viewCreate)
+route.get('/viewCreate-category', controller.viewCreate)
 
 //[POST] /products-category/create-category
 route.post(
@@ -24,5 +24,14 @@ route.post(
     validateCategory.createCategory,
     controller.create)
 
+//[GET] /products-category/edit/:id
+route.get('/edit/:id', controller.viewEdit)
+
+//[PATCH] /products-category/edit/:id
+route.patch('/edit/:id',
+    upload.single('thumbnail'),
+    middlewareUploadCloud.uploadCloud,
+    validateCategory.createCategory,
+    controller.edit)
 
 module.exports = route;
