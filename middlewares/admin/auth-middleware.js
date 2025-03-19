f3334ef7ac1bc3564791ced202d866ddc554d286
NgKien94 <quangkienofqn@gmail.com>
2025-03-19 21:09:47 +0700

    Authorization

--- middlewares/admin/auth-middleware.js
+++ middlewares/admin/auth-middleware.js
@@ -1,6 +1,7 @@
 const system = require('../../configs/system')
 const systemConfig = require('../../configs/system')
 const Account = require('../../models/account-model')
+const Role = require('../../models/role-model')
 
 module.exports.requireAuth = async (req,res,next) =>{
     if(!req.cookies.token){
@@ -10,13 +11,19 @@ module.exports.requireAuth = async (req,res,next) =>{
     else{
         const user = await Account.findOne({
             token: req.cookies.token
-        })
+        }).select("-password -token")
         
         if(!user){
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
             return;
         }
+        const role = await Role.findOne({
+            _id: user.role_id,
+            deleted: false
+        }).select("title permissions")
         
+        res.locals.user = user;
+        res.locals.role = role;
         next();
     }
     
